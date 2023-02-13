4583991ba45b6d0a9b6385a2cb85214f4a8cc471
Pol Milian <pol.milian.dev@gmail.com>
2023-02-13 14:12:47 +0800

    get example tweets

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
-async fn say_hi() -> impl Responder {
-    HttpResponse::Ok().body("Hello!")
+// API Tweets
+#[get("/tweets")]
+async fn get_tweets() -> HttpResponse {
+    let tweets = ["tweet 1: hello", "tweet 2: goodbye"];
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(tweets)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/test", web::get().to(say_hi)))
+    HttpServer::new(|| App::new().service(get_tweets))
         .bind(("127.0.0.1", 8000))?
         .run()
         .await
