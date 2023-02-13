a772a37bedca98619cc87dc85201cc76222a75bc
Pol Milian <pol.milian.dev@gmail.com>
2023-02-13 14:06:37 +0800

    add test route

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use actix_web::{App, HttpServer};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+async fn say_hi() -> impl Responder {
+    HttpResponse::Ok().body("Hello!")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new();
-    })
-    .bind(("127.0.0.1"), 8000)?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/test", web::get().to(say_hi)))
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
 }
