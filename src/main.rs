6f23f45bd545e20f07f3612b68b3779d0f7f5f88
Pol Milian <pol.milian.dev@gmail.com>
2023-02-16 16:38:19 +0800

    add connection pool behavior

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use actix_web::{App, HttpServer};
+use diesel::{
+    r2d2::{ConnectionManager, Pool},
+    PgConnection,
+};
+use std::env;
 
 mod constants;
 mod likes;
@@ -6,8 +11,16 @@ mod tweets;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not found");
+
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::builder()
+        .build(manager)
+        .expect("Could not create the pool");
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(pool.clone())
             .service(tweets::get_tweets)
             .service(tweets::create_tweet)
             .service(tweets::get_tweet_by_id)
