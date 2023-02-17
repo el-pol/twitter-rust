c83bdc87fe203ca220085efd9a86519471651d35
Pol Milian <pol.milian.dev@gmail.com>
2023-02-17 13:54:27 +0800

    add error handling

--- src/tweets.rs
+++ src/tweets.rs
@@ -32,9 +32,7 @@ impl Tweet {
 pub async fn get_tweets(pool: Data<Pool<ConnectionManager<PgConnection>>>) -> HttpResponse {
     use crate::schema::tweets::dsl::*;
 
-    let conn = pool
-        .get()
-        .expect(" No pude obtener conexión a la base de datos");
+    let conn = pool.get().expect("Could not connect to DB");
     let result = tweets
         .order(created_at.desc())
         .limit(10) // sería bueno parametrizar este valor
@@ -56,14 +54,12 @@ pub async fn create_tweet(
     pool: Data<Pool<ConnectionManager<PgConnection>>>,
 ) -> HttpResponse {
     let nuevo_tweet = Tweet::new(req_body);
-    let conn = pool
-        .get()
-        .expect(" No pude obtener conexión a la base de datos");
+    let conn = pool.get().expect("Could not connect to DB");
 
     diesel::insert_into(tweets::table)
         .values(&nuevo_tweet)
         .execute(&conn)
-        .expect("Error al insertar tweet");
+        .expect("Error inserting tweet");
 
     HttpResponse::Created()
         .content_type(APPLICATION_JSON)
@@ -76,14 +72,12 @@ pub async fn get_tweet_by_id(
     pool: Data<Pool<ConnectionManager<PgConnection>>>,
 ) -> HttpResponse {
     use crate::schema::tweets::dsl::*;
-    let conn = pool
-        .get()
-        .expect(" No pude obtener conexión a la base de datos");
+    let conn = pool.get().expect("Could not connect to DB");
     let t_id = &path.0 .0; // tweet id desde los parametros de la url
 
     let t_id_uuid = Uuid::from_str(t_id); // tweet id formateado a uuid
     if t_id_uuid.is_err() {
-        println!("tweet id inválido, error: {:?}", t_id_uuid.err());
+        println!("Invalid tweet id, error: {:?}", t_id_uuid.err());
         // si no pudimos convertir a un uuid válido, asumimos que el tweet no existe.
         return HttpResponse::NotFound().await.unwrap();
     }
